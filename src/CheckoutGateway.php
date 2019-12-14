4ad088f209e61d0d59fea4eaf3245a4dcbfa152f
GlennJenkins <glenn.jenkins@digitickets.co.uk>
2019-12-14 11:06:42 +0000

    Create the purchase request.

--- src/CheckoutGateway.php
+++ src/CheckoutGateway.php
@@ -2,12 +2,19 @@
 
 namespace DigiTickets\Stripe;
 
+use DigiTickets\Stripe\Messages\PurchaseRequest;
 use Omnipay\Common\AbstractGateway;
+use Omnipay\Common\Message\RequestInterface;
 
 class CheckoutGateway extends AbstractGateway
 {
     public function getName()
     {
         return 'Stripe (Checkout)';
+    }
+
+    public function purchase(array $parameters = []): RequestInterface
+    {
+        return $this->createRequest(PurchaseRequest::class, $parameters);
     }
 }
---
+++ src/Messages/PurchaseRequest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace DigiTickets\Stripe\Messages;
+
+use Omnipay\Common\Message\AbstractRequest;
+
+class PurchaseRequest extends AbstractRequest
+{
+    /**
+     * Get the gateway API Key (the "secret key").
+     *
+     * @return string
+     */
+    public function getApiKey(): string
+    {
+        return $this->getParameter('apiKey');
+    }
+
+    /**
+     * Set the gateway API Key.
+     *
+     * @return AbstractRequest provides a fluent interface.
+     */
+    public function setApiKey($value): AbstractRequest
+    {
+        return $this->setParameter('apiKey', $value);
+    }
+
+    public function getData()
+    {
+        // Just validate the parameters.
+        $this->validate('apiKey', 'transactionId', 'returnUrl', 'cancelUrl'/*, ???*/);
+
+        return null;
+    }
+
+    public function sendData($data)
+    {
+        // We use Stripe's SDK to initialise a (Stripe) session. The session gets passed through the process and is
+        // used to identify this transaction.
+        \Stripe\Stripe::setApiKey($this->getApiKey());
+
+        $session = \Stripe\Checkout\Session::create(
+            [
+                'client_reference_id' => $this->getTransactionId(),
+                'payment_method_types' => ['card'],
+                'line_items' => array_map(
+                    function(\Omnipay\Common\Item $item) {
+                        return [
+                            'name' => $item->getName(),
+                            'description' => $item->getDescription(),
+                            'amount' => 100*$item->getPrice(), // @TODO: The multiplier depends on the currency
+                            'currency' => $this->getCurrency(),
+                            'quantity' => $item->getQuantity(),
+                        ];
+                    },
+                    $this->getItems()->all()
+                ),
+                'success_url' => $this->getReturnUrl().'?session_id={CHECKOUT_SESSION_ID}',
+                'cancel_url' => $this->getCancelUrl(),
+            ]
+        );
+    }
+}
