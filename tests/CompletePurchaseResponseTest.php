8e223052a1313efad75cba1ebc9477d64aa0520c
GlennJenkins <glenn.jenkins@digitickets.co.uk>
2019-12-20 15:52:32 +0000

    More on the CompletePurchaseResponse test.

--- tests/CompletePurchaseResponseTest.php
+++ tests/CompletePurchaseResponseTest.php
@@ -16,31 +16,36 @@ class CompletePurchaseResponseTest extends TestCase
     {
         $request = Mockery::mock(CompletePurchaseRequest::class);
         $request->shouldReceive('getSessionID')->andReturn(self::SESSION_ID);
+        $successfulTxRef = sprintf('{"txRef":"%s","sessionId":"%s"}', PaymentIntent::PI_ID, self::SESSION_ID);
 
         return [
             'no payment intent' => [
                 $request,
                 [],
                 false,
-                'Could not retrieve payment'
+                'Could not retrieve payment',
+                sprintf('{"sessionId":"%s"}', self::SESSION_ID),
             ],
             'successful' => [
                 $request,
                 ['paymentIntent' => new PaymentIntent(PaymentIntent::PI_ID, CompletePurchaseResponse::STATUS_SUCCESS)],
                 true,
-                CompletePurchaseResponse::STATUS_SUCCESS
+                CompletePurchaseResponse::STATUS_SUCCESS,
+                $successfulTxRef,
             ],
             'canceled' => [
                 $request,
                 ['paymentIntent' => new PaymentIntent(PaymentIntent::PI_ID, CompletePurchaseResponse::STATUS_CANCELED)],
                 false,
-                'Canceled by customer'
+                'Canceled by customer',
+                $successfulTxRef,
             ],
             'unknown' => [
                 $request,
                 ['paymentIntent' => new PaymentIntent(PaymentIntent::PI_ID, 'Other')],
                 false,
-                'Unknown error'
+                'Unknown error',
+                $successfulTxRef,
             ],
         ];
     }
@@ -53,15 +58,20 @@ class CompletePurchaseResponseTest extends TestCase
      *
      * @dataProvider creationProvider
      */
-    public function testCreation(CompletePurchaseRequest $request, $data, bool $expectSuccess, string $expectMessage)
-    {
+    public function testCreation(
+        CompletePurchaseRequest $request,
+        $data,
+        bool $expectSuccess,
+        string $expectMessage,
+        string $expectedTransactionReference
+    ) {
         $completePurchaseResponse = new CompletePurchaseResponse($request, $data);
 
         $this->assertEquals($expectSuccess, $completePurchaseResponse->isSuccessful());
         $this->assertEquals($expectMessage, $completePurchaseResponse->getMessage());
         $this->assertNull($completePurchaseResponse->getCode());
 
-        $this->assertEquals('What is the correct value 1?', $completePurchaseResponse->getTransactionReference());
+        $this->assertEquals($expectedTransactionReference, $completePurchaseResponse->getTransactionReference());
         $this->assertEquals('What is the correct value 2?', $completePurchaseResponse->getTransactionId());
     }
 }
