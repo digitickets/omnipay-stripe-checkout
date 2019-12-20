821f368e9b1e2bcaf962a781ddc19650b575694e
GlennJenkins <glenn.jenkins@digitickets.co.uk>
2019-12-20 11:17:42 +0000

    Second half of the first unit test.

--- tests/ComplexTransactionRefTest.php
+++ tests/ComplexTransactionRefTest.php
@@ -41,4 +41,67 @@ class ComplexTransactionRefTest extends TestCase
             $this->assertEquals($refundReference, $transactionReference->getRefundReference());
         }
     }
+
+    public function buildFromJsonProvider()
+    {
+        $sessionID = 'S500';
+        $txRef = 'T600';
+        $refRef = 'R700';
+        return [
+            'just session' => [
+                sprintf('{"sessionId":"%s"}', $sessionID),
+                $sessionID,
+                null,
+                null,
+            ],
+            'session and txref' => [
+                sprintf('{"txRef":"%s","sessionId":"%s"}', $txRef, $sessionID),
+                $sessionID,
+                $txRef,
+                null,
+            ],
+            'session and refref' => [
+                sprintf('{"refundRef":"%s","sessionId":"%s"}', $refRef, $sessionID),
+                $sessionID,
+                null,
+                $refRef,
+            ],
+            'all three' => [
+                sprintf('{"refundRef":"%s","txRef":"%s","sessionId":"%s"}', $refRef, $txRef, $sessionID),
+                $sessionID,
+                $txRef,
+                $refRef,
+            ],
+        ];
+    }
+
+    /**
+     * @param string $jsonString
+     * @param string $expectedSessionID
+     * @param string|null $expectedTxRef
+     * @param string|null $expectedRefRef
+     *
+     * @dataProvider buildFromJsonProvider
+     */
+    public function testBuildFromJson(
+        string $jsonString,
+        string $expectedSessionID,
+        string $expectedTxRef = null,
+        string $expectedRefRef = null
+    ) {
+        $transactionReference = ComplexTransactionRef::buildFromJson($jsonString);
+
+        // @TODO: I should be able to combine these assertions with the ones in the other test. Ie have a separate method that does the tests.
+        $this->assertEquals($expectedSessionID, $transactionReference->getSessionID());
+        if (is_null($expectedTxRef)) {
+            $this->assertNull($transactionReference->getTransactionReference());
+        } else {
+            $this->assertEquals($expectedTxRef, $transactionReference->getTransactionReference());
+        }
+        if (is_null($expectedRefRef)) {
+            $this->assertNull($transactionReference->getRefundReference());
+        } else {
+            $this->assertEquals($expectedRefRef, $transactionReference->getRefundReference());
+        }
+    }
 }
